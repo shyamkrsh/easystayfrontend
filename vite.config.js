212cec3c837d78e6459ff0dc5b01a3dd40c02039
Shyam Kumar Sharma <136264959+shyamkrsh@users.noreply.github.com>
2024-11-13 09:03:47 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
- 
+  server: {
+  proxy: {
+    '/api': 'https://easystaybackend.onrender.com'
+  }
+ },
   plugins: [react()],
 })
