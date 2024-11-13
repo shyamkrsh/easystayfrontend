0d9226d9dc1a8e2036238b54c023c63760bdd9b3
Shyam Kumar Sharma <sharmashyamkumar717@gmail.com>
2024-11-13 12:46:19 +0530

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
- server: {
-  proxy: {
-    '/api': {
-      target: 'https://easystaybackend.onrender.com',
-      changeOrigin: true,
-      secure: false,  // Use `false` if you're testing with an HTTP server (not recommended for production)
-      rewrite: (path) => path.replace(/^\/api/, '') // Rewrite `/api` to remove the prefix before forwarding
-    }
-  }
-},
   plugins: [react()],
 })
