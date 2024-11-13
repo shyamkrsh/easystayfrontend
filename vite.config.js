6fde8b83dbaad82164dbe6d341abd6487218e463
Shyam Kumar Sharma <sharmashyamkumar717@gmail.com>
2024-11-13 13:22:00 +0530

    added proxy bu no rewrite rule

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
       '/api': {
         target: 'https://easystaybackend.onrender.com', // Your backend URL
         changeOrigin: true,  // To avoid CORS issues
-        secure: false,       // Set to false if you’re using HTTP
+        secure: true,       // Set to false if you’re using HTTP
       }
     }
   },
