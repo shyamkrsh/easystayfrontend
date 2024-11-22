44dfeead29c21421530e818ffc3d616d74e77c78
Shyam Kumar Sharma <sharmashyamkumar717@gmail.com>
2024-11-22 14:38:45 +0530

    search fixed

--- src/Components/listings/Listings.jsx
+++ src/Components/listings/Listings.jsx
@@ -16,9 +16,12 @@ function Listings() {
   const [search, setSearch] = useState("");
   const [showListen, setShowListen] = useState(false);
 
-  let string = 'shyam, kumar sharma , bhabhata, bihar-800013';
-  let ans = string.replaceAll(",", "").replaceAll(" ", "").replaceAll("-", "").toUpperCase();
-  console.log(ans);
+
+  let str1 = "search balupur";
+  let str2 = "patna, balupur, bihar"
+
+
+
 
   const { category } = useParams();                               
 
@@ -55,11 +58,10 @@ function Listings() {
         {
           search ?
             listings?.map((item) => (
-              search && item?.location.replaceAll(",", "").replaceAll(" ", "").replaceAll("-", "").toUpperCase().includes(search.replaceAll(",", "").replaceAll(" ", "").replaceAll("-", "").toUpperCase()) ?
+              search && search.replaceAll(",", "").toLowerCase().split(" ").some((word) => item.location.replaceAll(",", "").toLowerCase().includes(word)) ?
                 <Link to={`/listings/show/${item?._id}`} key={item?._id}>
                   <Card key={item?._id} image={item?.images} title={item?.title} price={item?.price} description={item?.description} street_address={item?.location} />
                 </Link>
-
                 : null
             ))
             :
